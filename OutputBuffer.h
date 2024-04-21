e3451e8679c6441ba8d6a3b2f53cb46b87cb7b06
zsf <501942314@qq.com>
2024-04-21 22:40:52 +0800

    begin of tinker, a utility library.

---
+++ OutputBuffer.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+namespace TK
+{
+	template<int N = 0>
+	class FOutputBuffer
+	{
+		static_assert(N >= 0);
+		using FByte = char;
+		
+	public:
+
+		bool Push(const void* src, int len)
+		{
+			if(len <= 0)
+				return true;
+
+			int Required = Size + len;
+			if(Required < Size)
+				return false;
+
+			if(Capacity < Required)
+			{
+				const int Suggested = Size + Size / 2;
+				Capacity = Suggested > Required? Suggested : Required;
+
+				if(Heap)
+				{
+					//Heap = static_cast<FByte*>(std::realloc(Heap, Capacity));
+				}
+			}
+		}
+
+	private:
+
+		int Size {0};
+		int Capacity {N};
+
+		FByte InlinedBuffer[N];
+		FByte* Heap {nullptr};
+	};
+}
